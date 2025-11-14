026cc8a46cff923e0dcba8d466f7abe6fd4d7054
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-11-14 19:56:40 +0530

    Update CORS and API endpoints to use production URL

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://vishaljobportal.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
